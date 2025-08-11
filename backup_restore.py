029714fb4bc4349e67179c54f16a7df99e4a2f11
Sachin Gorai <sachingorai257@gmail.com>
2025-08-11 22:08:03 +0530

    backup_restore.py

--- backup_restore.py
+++ backup_restore.py
@@ -2,8 +2,8 @@ import shutil
 
 def backup_db():
     shutil.copyfile('finance.db', 'finance_backup.db')
-    print("🔄 Backup completed.")
+    print("Backup completed.")
 
 def restore_db():
     shutil.copyfile('finance_backup.db', 'finance.db')
-    print("✅ Restore completed.")
+    print("Restore completed.")
